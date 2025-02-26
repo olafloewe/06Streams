fb1b7c3b76faa2fc60034b9cf2809c942a11b4ae
Olaf Loewe <olafloewe95@gmail.com>
2025-02-26 16:18:36 +0100

    Finished

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,44 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace Task_1 {
     internal class Program {
         
         static void Main(string[] args) {
-            SaveFile(Listener());
+            SaveFile();
         }
 
-        private static String Listener() {
-            String log= "";
-            bool write = false;
-
-            // checks when the user wants to start logging
-            while (!write) {
-                write = (Console.ReadLine().ToLower() == "start");
-            }
-
-            // logs while input is not STOP
-            while (write) {
-                String writtenLine = Console.ReadLine();
-                if (writtenLine.ToLower() == "stop") break;
-
-                Console.WriteLine(writtenLine); // output writtenLine
-            }
-
-            return log;
-        }
-
-        private static void SaveFile(String arg) {
+        private static void SaveFile() {
             DateTime today = DateTime.Today;
-            string fileName = today.ToString("d");
-            string path = $@"../../";
 
-            using (StreamWriter outputFile = new StreamWriter(Path.Combine(path, fileName))) {
-                outputFile.WriteLine(arg);
+            string fileName = today.ToString("d").Replace("/",".");
+            Console.WriteLine(fileName);
+
+            string docPath = AppDomain.CurrentDomain.BaseDirectory;
+            Console.WriteLine(docPath);
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, $"{fileName}.txt"))) {
+                bool write = false;
+                outputFile.WriteLine($"Captain's Log\nStartdate {fileName}\n");
+
+                // checks when the user wants to start logging
+                while (!write) {
+                    write = (Console.ReadLine().ToLower() == "start");
+                }
+
+                // logs while input is not STOP
+                while (write) {
+                    String writtenLine = Console.ReadLine();
+                    if (writtenLine.ToLower() == "stop") break;
+
+                    outputFile.WriteLine(writtenLine);
+                }
+
+                outputFile.WriteLine("\nJean-Luc Picard");
             }
         }
     }
