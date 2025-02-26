f4e4463b192d91b1cc264d7cefce5a9785d179a1
Olaf Loewe <olafloewe95@gmail.com>
2025-02-26 15:56:42 +0100

    Second itteration

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,26 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Task_1 {
     internal class Program {
+        
         static void Main(string[] args) {
-            while (Console.ReadLine().ToLower() != "start") {
-                if (Console.ReadLine().ToLower() != "stop") break;
-                writtenLine = Console.ReadLine();
-                Console.WriteLine(writtenLine);
-            }
+            SaveFile(Listener());
         }
 
-        string writtenLine;
+        private static String Listener() {
+            String log= "";
+            bool write = false;
 
-        private static void Listener() {
-            while (Console.ReadLine().ToLower() != "start") {
-                if (Console.ReadLine().ToLower() != "stop") break;
-                writtenLine = Console.ReadLine();
-                Console.WriteLine(writtenLine);
+            // checks when the user wants to start logging
+            while (!write) {
+                write = (Console.ReadLine().ToLower() == "start");
+            }
+
+            // logs while input is not STOP
+            while (write) {
+                String writtenLine = Console.ReadLine();
+                if (writtenLine.ToLower() == "stop") break;
+
+                Console.WriteLine(writtenLine); // output writtenLine
+            }
+
+            return log;
+        }
+
+        private static void SaveFile(String arg) {
+            DateTime today = DateTime.Today;
+            string fileName = today.ToString("d");
+            string path = $@"../../";
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(path, fileName))) {
+                outputFile.WriteLine(arg);
             }
         }
     }
